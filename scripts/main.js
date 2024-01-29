7dc56ad8f9770d11d29a57c3be78c25f3c7a3ec2
mimiperezg <perez-gerus.m@northeastern.edu>
2024-01-29 15:30:19 -0500

    fixed bnnnuy issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,21 @@
 let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello friend you are invited mimis birthday party";
+myHeading.textContent = "Hello friend, you are invited to Mimi's birthday party";
 
 let myImage = document.getElementById('my-bnnuy');
-let myInvite = document.getElementById('my-invite');
+let myButton = document.querySelector("button");
 
 myButton.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "https://i.ibb.co/KjhnkvJ/image.jpg") {
-      myImage.setAttribute("src", "images/image2.jpeg");
+    myImage.setAttribute("src", "images/image.jpeg");
   } else {
-      myImage.setAttribute("src", "https://i.ibb.co/KjhnkvJ/image.jpg");
+    myImage.setAttribute("src", "https://i.ibb.co/KjhnkvJ/image.jpg");
   }
-};
+}; 
 
 function displayBnnuy() {
-    const image = document.getElementById('my-bnnuy');
-    image.style.display = 'block';
+  const image = document.getElementById('my-bnnuy');
+  image.style.display = 'block';
 }
 
 // Event listener for button click to display image
@@ -25,18 +25,13 @@ document.getElementById('bnnuy-button').addEventListener('click', function() {
 
 // Separate function for myInvite button
 function setUserName() {
-  let myName = prompt('ur name plz');
+  let myName = prompt('Enter your name:');
   if (!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Hello ' + myName + ' you are invited mimis birthday party';
+    myHeading.innerHTML = 'Hello ' + myName + ', you are invited to mimi\'s birthday party';
   }
-}
-
-// Event listener for myInvite button click
-myInvite.onclick = function() {
-  setUserName();
 }
 
 // Call setUserName function when the page loads
